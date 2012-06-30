bb04214083af227ad65d99d22486e3df4d0d7ea3
Tom Crayford <tcrayford@googlemail.com>
2012-06-30 16:30:02 +0100

    refactor

---
+++ lib/raffle/refactorings/extract_method.rb
@@ -0,0 +1,71 @@
+class Array
+  def comments
+    []
+  end
+  
+  def sexp_type
+    first
+  end
+
+  def to_sexp
+    Sexp.from_array(Rubinius::AST::Block.new(first.line, self).to_sexp)
+  end
+end
+
+module Raffle
+  module Serializer
+    def self.to_ruby(ast)
+      Ruby2Ruby.new.process(ast.to_sexp)
+    end
+  end
+
+  class FindsBodies
+    def self.find(method_definition, lines)
+      body = method_definition.body.array
+      body.select { |i| lines.include? i.line }
+    end
+  end
+
+  class CreatesMethods
+    def self.create_new_method(new_method_name, body)
+      code = <<-RUBY
+def #{new_method_name}
+#{Serializer.to_ruby(body)}
+end
+RUBY
+Serializer.to_ruby(code)
+    end
+  end
+
+
+  module Refactorings
+    class ExtractMethod
+      def self.extract_method(code, new_method_name, start_line, end_line, start_column, end_column)
+        new(code, new_method_name, start_line..end_line, start_column..end_column).extract_method
+      end
+
+      def initialize(code, new_method_name, lines, _)
+        @code = code
+        @new_method_name = new_method_name
+        @lines = lines
+      end
+
+      def extract_method
+        ast = @code.to_ast
+
+        body = FindsBodies.find(ast, @lines)
+
+        extracted = CreatesMethods.create_new_method(@new_method_name, body)
+        original = replace_body(ast, @lines, @new_method_name)
+
+        "#{extracted}\n\n#{original}"
+      end
+
+      def replace_body(method_definition, lines, new_method_name)
+        method_definition.body.array = ["#{new_method_name}()".to_ast]
+        Serializer.to_ruby(method_definition)
+      end
+    end
+  end
+end
+
--- spec/end_to_end/extract_method_rbx_spec.rb
+++ spec/end_to_end/extract_method_rbx_spec.rb
@@ -1,53 +1,9 @@
 require 'pp'
 require 'ruby2ruby'
-
-class Array
-  def comments
-    []
-  end
-  
-  def sexp_type
-    first
-  end
-
-  def to_sexp
-    Sexp.from_array(Rubinius::AST::Block.new(first.line, self).to_sexp)
-  end
-end
+$LOAD_PATH.unshift(File.expand_path("../../../lib", __FILE__))
+require 'raffle/refactorings/extract_method'
 
 describe 'extracting a method' do
-  def to_ruby(ast)
-    Ruby2Ruby.new.process(ast.to_sexp)
-  end
-
-  def find_body(method_definition, start_line, end_line)
-    body = method_definition.body.array
-    body.select { |i| (start_line..end_line).include? i.line }
-  end
-
-  def create_new_method(new_method_name, body)
-    code = <<-RUBY
-def #{new_method_name}
-  #{to_ruby(body)}
-end
-RUBY
-    to_ruby(code)
-  end
-
-  def replace_body(method_definition, start_line, end_line, new_method_name)
-    method_definition.body.array = ["#{new_method_name}()".to_ast]
-    to_ruby(method_definition)
-  end
-
-  def extract_method(code, new_method_name, start_line, end_line, start_column, end_column)
-    ast = code.to_ast
-    body = find_body(ast, start_line, end_line)
-    n = create_new_method(new_method_name, body)
-    p = replace_body(ast, start_line, end_line, new_method_name)
-
-    "#{n}\n\n#{p}"
-  end
-
   it 'should extract without any local variables or args' do
     code = <<-RUBY
 def foo
@@ -63,6 +19,7 @@ def foo
   bar
 end
 RUBY
-    extract_method(code, 'bar', 2, 2, 0, 12).strip.should == extracted_code.strip
+    Raffle::Refactorings::ExtractMethod.extract_method(code, 'bar', 2, 2, 0, 12).strip.should == extracted_code.strip
   end
 end
+
