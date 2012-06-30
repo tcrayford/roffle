a21d092d64d9921c537a24b802b011707fdb96b6
Tom Crayford <tcrayford@googlemail.com>
2012-06-30 12:51:48 +0100

    add end to end spec

--- spec/end_to_end/extract_method_spec.rb
+++ spec/end_to_end/extract_method_spec.rb
@@ -1,10 +1,22 @@
-describe 'extrating a method' do
+describe 'extracting a method' do
+  def extract_method(code, new_method_name, start_line, end_line, start_column, end_column)
+  end
+
   it 'should extract without any local variables or args' do
     code = <<-RUBY
 def foo
   puts "123"
 end
-    RUBY
-    extract_method(code, 2, 0, 12)
+RUBY
+    extracted_code = <<-RUBY
+def bar
+  puts "123"
+end
+
+def foo
+  bar
+end
+RUBY
+    extract_method(code, 'bar', 2, 2, 0, 12).should == extracted_code
   end
 end
